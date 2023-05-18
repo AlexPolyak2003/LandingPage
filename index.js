8b245227b749be51cd26628011b71fd24810ed8d
Alexandr <101368178+AlexPolyak2003@users.noreply.github.com>
2023-05-18 18:44:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,15 @@
-
-
 $(document).ready(function(){
 	var lpn = new LPNavigator('.screen');
-	// const block = document.getElementById('block'),
-	// 	blockH = document.getElementById('block').clientHeight
 	
 	$('#navLogo').click(function(){
 		lpn.scrollTo(0);
 	});
 	
-
-
 	lpn.onShowScreen = function(idx) {
-		let scroll = window.scrollY
-		
-		
-		// if (idx > 0)
-
-		if (idx > 0)
-		$('.navbar').addClass('smooth-show').removeClass('smooth-hide');
-		else $('.navbar').addClass('smooth-hide').removeClass('smooth-show');
-
-
-		
-		
-		
-		// $('.navbar').removeClass('smooth-hide').addClass('smooth-show');
-		// else $('.navbar').removeClass('smooth-show').addClass('smooth-hide');
-
+		if (idx > 0) $('.navbar').removeClass('smooth-hide').addClass('smooth-show');
+		else $('.navbar').removeClass('smooth-show').addClass('smooth-hide');
+	};
 	
-		
-		// $('.navbar').removeClass('smooth-hide')
-		
-		
-		// .addClass('smooth-hide');
-		// else $('.navbar').removeClass('smooth-hide').addClass('smooth-show');
- 		
-		// if (scroll < 464)
-		// $('.navbar').removeClass('smooth-show').addClass('smooth-hide')
-
-
-
-	}
-	
-	lpn.onShowScreen(); // handle refresh for scrolled down page
-
-
-
-
-
+	lpn.onShowScreen(lpn.screenIndex); // handle refresh for scrolled down page
 });
+
